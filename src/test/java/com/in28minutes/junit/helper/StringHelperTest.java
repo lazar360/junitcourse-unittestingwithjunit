880517afdfa3e7cbc99836be64ec27a4e0559b23
NicolasGautier <nicolas.gautier35135@gmail.com>
2023-08-03 11:49:11 +0200

    maj test step 4 finished

--- src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
+++ src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -4,18 +4,27 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class StringHelperTest {
-
+    StringHelper helper = new StringHelper();
+    // AACD = > CD ACD => CD CDEF => CDEF CDAA => CDAA
     @org.junit.Test
-    public void truncateAInFirst2Positions() {
-        StringHelper helper = new StringHelper();
-        String actual = helper.truncateAInFirst2Positions("AACD");
-        String expected = "CD";
-        // AACD = > CD ACD => CD CDEF => CDEF CDAA => CDAA
-        assertEquals(expected, actual);
+    public void testTruncateAInFirst2Positions_AinFirst2Positions() {
+        assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+    }
+    @org.junit.Test
+    public void testTruncateAInFirst2Positions2_AinFirstPosition() {
+        assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+    }
+    @org.junit.Test
+    public void testTruncateAInFirst2Positions2_NoAinFirstPosition() {
+        assertEquals("CDEF", helper.truncateAInFirst2Positions("CDEF"));
+    }
+    @org.junit.Test
+    public void testTruncateAInFirst2Positions2_Ain2LastPositions() {
+        assertEquals("CDAA", helper.truncateAInFirst2Positions("CDAA"));
     }
 
-    @org.junit.Test
-    public void areFirstAndLastTwoCharactersTheSame() {
-        fail("Not yet implemented");
-    }
+//    @org.junit.Test
+//    public void areFirstAndLastTwoCharactersTheSame() {
+//        fail("Not yet implemented");
+//    }
 }
